fe608c39d72a3e3f7dceb0fe492c03756dff611e
mohammad <m.tabeemam@gmail.com>
2020-10-11 18:47:03 +0330

    add Movie Items to repo

--- app/src/main/java/com/app/avatar/data/source/Repository.kt
+++ app/src/main/java/com/app/avatar/data/source/Repository.kt
@@ -1,9 +1,32 @@
 package com.app.avatar.data.source
 
+import androidx.lifecycle.LiveData
+import com.app.avatar.data.model.MovieItem
 import com.app.avatar.data.source.local.Database
+import com.app.avatar.data.source.remote.MovieItemApiService
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import timber.log.Timber
+import java.lang.Exception
 
 class Repository(
     private val database: Database
 ) {
 
+    suspend fun refreshMovieItems() {
+        return try {
+            val result = MovieItemApiService.api.getMoviesListAsync().await()
+            refreshLocalDataSource(result)
+        } catch (e: Exception) {
+            Timber.e(e)
+        }
+    }
+
+    private suspend fun refreshLocalDataSource(marvelList: List<MovieItem>) =
+        withContext(Dispatchers.IO) {
+            database.movieItemDao().clear()
+            database.movieItemDao().insertAll(marvelList)
+        }
+
+    fun observeList(): LiveData<List<MovieItem>> = database.movieItemDao().observeAll()
 }
--- app/src/main/java/com/app/avatar/data/source/remote/MovieItemApi.kt
+++ app/src/main/java/com/app/avatar/data/source/remote/MovieItemApi.kt
@@ -1,6 +1,7 @@
 package com.app.avatar.data.source.remote
 
 import com.app.avatar.data.model.Movie
+import com.app.avatar.data.model.MovieItem
 import kotlinx.coroutines.Deferred
 import retrofit2.http.GET
 import retrofit2.http.Query
@@ -11,9 +12,9 @@ object MovieItemApiService {
 
 interface MovieItemApi {
     @GET
-    fun getMoviesList(
+    fun getMoviesListAsync(
         @Query("apiKey") apiKey: String = Network.API_KEY,
         @Query("s") search: String = Network.SEARCH
-    ): Deferred<List<Movie>>
+    ): Deferred<List<MovieItem>>
 }
 
