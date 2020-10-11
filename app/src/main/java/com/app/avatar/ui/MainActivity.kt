ab8264308aebedd0ed5c79e023b8c6f23009646b
mohammad <m.tabeemam@gmail.com>
2020-10-11 17:49:19 +0330

    add navigation component

--- app/src/main/java/com/app/avatar/ui/MainActivity.kt
+++ app/src/main/java/com/app/avatar/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.app.avatar
+package com.app.avatar.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.app.avatar.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
