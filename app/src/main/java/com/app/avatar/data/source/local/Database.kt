d1f69938ef304d3a7ab03c6d9d7eb73f3b34bdb9
mohammad <m.tabeemam@gmail.com>
2020-10-11 18:12:56 +0330

    add models

--- app/src/main/java/com/app/avatar/data/source/local/Database.kt
+++ app/src/main/java/com/app/avatar/data/source/local/Database.kt
@@ -2,10 +2,11 @@ package com.app.avatar.data.source.local
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.app.avatar.data.model.Movie
 import com.app.avatar.data.model.MovieItem
 
 @Database(
-    entities = [MovieItem::class],
+    entities = [MovieItem::class, Movie::class],
     version = 1,
     exportSchema = false
 )
